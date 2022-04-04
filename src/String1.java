c70fb5643543e9011e1b6e777c691a2eed56a7c1
TimNilsson04 <tim.nilsson2@elev.ga.ntig.se>
2022-04-04 11:17:13 +0200

    Initial commit

---
+++ src/String1.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class String1 {
+    public static void main(String[] args) {
+        String a = JOptionPane.showInputDialog("Vilken text vill du reversera?");
+       String b = reverse(a);
+
+         System.out.println(b);
+    }
+    public static String reverse(String a) {
+
+        String c = "";
+        for (int i = a.length(); i > 0; i--) {
+
+            c = c + a.charAt(i-1);
+        }
+            return c;
+    }
+}
---
+++ src/String2.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+public class String2 {
+    public static void main(String[] args) {
+        String a = JOptionPane.showInputDialog("Vilken text vill du CamelCasa?");
+        String b = camel();
+    }
+    public String Camel(String a){
+
+        String c = "";
+
+        for (int i = 0; i < a.length(); i++){
+
+            if(a.charAt(i) == ' '){
+                
+
+                if (a.charAt(i) == ' '){
+                    i++;
+                    String c = toUppercase
+
+
+                }
+
+            }
+        }
+
+    }
+
+}
